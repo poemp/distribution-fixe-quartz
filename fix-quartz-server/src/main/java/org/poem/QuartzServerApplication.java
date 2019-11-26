636a6f8762f8b28a56ac3b1b56ed14c2f3f54575
lywwrking <xue_2013@sina.com>
2019-11-26 21:51:04 +0800

    宝宝猪

--- fix-quartz-server/src/main/java/org/poem/QuartzServerApplication.java
+++ fix-quartz-server/src/main/java/org/poem/QuartzServerApplication.java
@@ -1,5 +1,6 @@
 package org.poem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -16,12 +17,29 @@ public class QuartzServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuartzServerApplication.class, args);
+        QuartzServerApplication quartzServerApplication = new QuartzServerApplication();
+        quartzServerApplication.done();
     }
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public  RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    private void done(){
+        String s = restTemplate.getForEntity("http://www.jiaobu365.com/payn/order/pid/34635.html",String.class).getBody();
+        System.out.println(s);
+
     }
 
 }
